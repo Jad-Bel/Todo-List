6291fdadcc2ddaa696eed4e2d1676ce1531aff6b
Jad-Bel <jadthegamer06@gmail.com>
2024-10-30 15:17:38 +0100

    Still working on task counter for each card

--- public/todo.js
+++ public/todo.js
@@ -1,11 +1,22 @@
+let taskCounter = 1;
+
 document.getElementById('openModal').addEventListener("click", function () {
     document.getElementById('modal').style.display = 'block';
+    clearModalFields();
 });
 
 document.getElementById('closeModal').addEventListener("click", function () {
     document.getElementById('modal').style.display = 'none';
 });
 
+function clearModalFields() {
+    document.getElementById('modalTaskTitle').value = "";
+    document.getElementById('modalTaskDescription').value = "";
+    document.getElementById('modalTaskDate').value = "";
+    document.getElementById('modalTastStatus').value = "P1";
+    document.getElementById('modalTaskStatus').value = "todo";
+    document.getElementById('submitTask').dataset.editing = "false"
+}
 
 document.getElementById('submitTask').addEventListener("click", function () {
 
@@ -15,6 +26,7 @@ document.getElementById('submitTask').addEventListener("click", function () {
     let taskStatus = document.getElementById('modalTaskStatus').value;
     let taskType = document.getElementById('modalTaskType').value;
     let priority = document.getElementById('modalTaskStatus').value;
+
 
     if (taskTitle !== "") {
         let newTask = document.createElement('li');
@@ -30,18 +42,34 @@ document.getElementById('submitTask').addEventListener("click", function () {
 
 
         newTask.innerHTML = `
+        <div class="">
             <h3 class="font-semibold text-center pr-2">${taskTitle}</h3>
-            <p class="text-left pr-2">${taskDescription}</p>
-            <p class="text-left pr-2"><strong>Date:</strong> ${taskDate}</p>
-            <p class="text-left pr-2"><strong>Status:</strong> ${taskStatus}</p>
+            <div>
+                <p class="text-left pr-2">${taskDescription}</p>
+                <p class="text-left pr-2"><strong>Date:</strong> ${taskDate}</p>
+                <p class="text-left pr-2"><strong>Status:</strong> ${taskStatus}</p>
+            </div>
+            <div class="flex justify-end">
+                <span class="icon-[proicons--pencil]  mr-2 mb-2 cursor-pointer"></span>
+            </div>
+        </div>
         `;
 
+        taskCounter++;
+
+        let taskList;
         if(taskType === "todo") {
-            document.getElementById('todoList').appendChild(newTask);
+            taskList = document.getElementById('todoList').appendChild(newTask);
         } else if (taskType === "doing") {
-            document.getElementById('doingList').appendChild(newTask);
+            taskList = document.getElementById('doingList').appendChild(newTask);
         } else if (taskType === "Done"){
-            document.getElementById('doneList').appendChild(newTask);
+            taskList = document.getElementById('doneList').appendChild(newTask);
+        }
+
+        if (taskStatus === "P1") {
+            taskList.prepend(newTask);
+        } else {
+            taskList.appendChild(newTask);
         }
 
         document.getElementById('modalTaskTitle').value = "";
